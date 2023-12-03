7e5123371d738c0bb86b628e291782be262b75cc
cpuntier <cpuntier@gmail.com>
2023-12-03 10:52:43 -0500

    ALAB complete

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function addArray(array) {
     return sum;
 };
 
-console.log(addArray(numArray));
+// console.log(addArray(numArray));
 
 // Take an array of numbers and return the average.
 function avgArray(array) {
@@ -20,7 +20,7 @@ function avgArray(array) {
     return avg;
 }
 
-console.log(avgArray(numArray));
+// console.log(avgArray(numArray));
 
 
 
@@ -39,7 +39,7 @@ function longString(array) {
     return longest;
 }
 
-console.log(longString(stringArray));
+// console.log(longString(stringArray));
 
 // Take an array of strings, and a number and return an array of the strings that are longer than the given number. 
 function stringsLongerThan(array, size) {
@@ -52,7 +52,7 @@ function stringsLongerThan(array, size) {
     return output;
 
 }
-console.log(stringsLongerThan(stringArray, 0));
+// console.log(stringsLongerThan(stringArray, 0));
 
 
 // Take a number, n, and print every number between 1 and n without using loops. Use recursion.
@@ -60,24 +60,84 @@ console.log(stringsLongerThan(stringArray, 0));
 
 //////PART 2//////
 
-let peopleArray = [{ id: "42", name: "Bruce", occupation: "Knight", age: "41" }, 
-{ id: "48", name: "Barry", occupation: "Runner", age: "25" }, 
-{ id: "57", name: "Bob", occupation: "Fry Cook", age: "19" }, 
-{ id: "63", name: "Blaine", occupation: "Quiz Master", age: "58" }, 
+let peopleArray = [{ id: "42", name: "Bruce", occupation: "Knight", age: "41" },
+{ id: "48", name: "Barry", occupation: "Runner", age: "25" },
+{ id: "57", name: "Bob", occupation: "Fry Cook", age: "19" },
+{ id: "63", name: "Blaine", occupation: "Quiz Master", age: "58" },
 { id: "7", name: "Bilbo", occupation: "None", age: "111" }];
 
 // Sort the array by age.
-console.log(peopleArray.sort());
+// console.log(peopleArray.sort());////////////////////////////////
 peopleArray.sort((a, b) => parseInt(a.age) - parseInt(b.age));
-console.log(peopleArray);
+// console.log(peopleArray);/////////////////////////////////////////////
 
 
 // Filter the array to remove entries with an age greater than 50.
+// console.log(peopleArray.filter(people => people.age <= 50));//////////////////////
 
 
 // Map the array to change the “occupation” key to “job” and increment every age by 1.
 
 
+let mapArray = peopleArray.map((person) => {
+    person.age = parseInt(person.age) + 1;
+    person["job"] = person["occupation"];
+    delete person.occupation;
+    return person;
+}
+)
+
+console.log(mapArray);
+
+
 // Use the reduce method to calculate the sum of the ages.
 
-// Then use the result to calculate the average age.
+const agesSum = peopleArray.reduce(
+    (accumulator, currentValue) => accumulator + parseInt(currentValue.age),
+    0,
+)
+
+let avgAge = agesSum / peopleArray.length;
+console.log(agesSum);
+console.log(avgAge);
+
+
+//Write function that take object and increment age
+testObject = {
+    name: "chris",
+    age: 49,
+}
+
+//Modifies object taken in
+function incrementAge(object){
+    if(object.age || object.age ===0){
+        object.age = object.age+ 1;
+    }else{
+        object.age = 0;
+    }
+    object.updated_at = Date.now();
+}
+console.log(testObject);
+incrementAge(testObject, testObject.age);
+console.log(testObject);
+
+
+
+//Take an object, make a copy, and increment the age field of the copy. Return the copy.
+function copyIncrementAge(object){
+    output = {}
+    for(i in object){
+        output[i]= object[i];
+    }
+    if(output.age || output.age === 0){
+        output.age = output.age +1;
+    }else{
+        output.age = 0;
+    }
+    output.updated_at = Date.now();
+    return output;
+}
+console.log("test object before copy",testObject);
+copyObject = copyIncrementAge(testObject);
+console.log("copied object ",copyObject);
+console.log("test object after copy is made", testObject);
